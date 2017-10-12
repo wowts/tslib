0700fc0fc24ea91261b151c8dead2329c30771a7
Sidoine De Wispelaere <sidoine@sidoine.net>
2017-10-12 09:35:45 +0200

    Initial import

---
+++ index.ts
@@ -0,0 +1,31 @@
+import { _G, setmetatable } from "@wowts/lua";
+
+export type Constructor<T> = new(...argv: any[]) => T;
+
+export interface Library<T> {
+    Embed<U>(base: Constructor<U>): Constructor<T & U>;
+}
+
+export function newClass(base: any, prototype: any) {
+    const c = prototype;
+    if (base) {
+        if (!base.constructor) {
+            base.constructor = () => {};
+        }
+    } else {
+        if (!c.constructor)  {
+            c.constructor = () => {};
+        }
+    }
+
+    c.__index = c;
+    setmetatable(c, <any> {
+        __call: (cls, ...args: any[]) => {
+            const self = <{constructor(...args: any[]): void}> setmetatable({}, cls);
+            self.constructor(...args);
+            return self;
+          },
+          __index: base,
+    });
+    return c;
+}
