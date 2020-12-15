d6c352259859eed9024a0179c808538299fe72c3
Sidoine De Wispelaere <sidoine@sidoine.net>
2020-12-15 13:15:12 +0100

    feat: add ternaryWrap and ternaryUnwrap

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { setmetatable } from "@wowts/lua";
 
-export type Constructor<T> = new(...argv: any[]) => T;
+export type Constructor<T> = new (...argv: any[]) => T;
 
 export interface Library<T> {
     Embed<U>(base: Constructor<U>): Constructor<T & U>;
@@ -13,20 +13,37 @@ export function newClass(base: any, prototype: any) {
             base.constructor = () => {};
         }
     } else {
-        if (!c.constructor)  {
+        if (!c.constructor) {
             c.constructor = () => {};
         }
     }
 
     c.__index = c;
-    setmetatable(c, <any> {
+    setmetatable(c, <any>{
         // tslint:disable-next-line:variable-name
         __call: (cls: any, ...__args: any[]) => {
-            const self = <{constructor(...args: any[]): void}> setmetatable({}, cls);
+            const self = <{ constructor(...args: any[]): void }>(
+                setmetatable({}, cls)
+            );
             self.constructor(...__args);
             return self;
         },
         __index: base,
     });
     return c;
+}
+
+let nilWrap = {};
+let falseWrap = {};
+
+export function ternaryWrap(value: any) {
+    return (
+        (value === null && nilWrap) || (value === false && falseWrap) || value
+    );
+}
+
+export function ternaryUnwrap(value: any) {
+    if (value === nilWrap) return undefined;
+    else if (value === falseWrap) return false;
+    else return value;
 }
